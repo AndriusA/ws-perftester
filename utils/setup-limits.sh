37905d5e2f6fc4f02b0a4911a912c94a8f581b3a
AndriusA <andrius.aucinas@gmail.com>
2014-03-12 17:52:18 +0000

    pushing sysctl config to clients when setting up fd limits

--- utils/setup-limits.sh
+++ utils/setup-limits.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 inst=(
-"ec2-SOMETHINGSOMETHING"
+
 )
 
 for i in "${inst[@]}";
@@ -9,8 +9,10 @@ do
   {
     listener="ubuntu@"$i
     echo $listener
-    scp limits.conf $listener":."
+    rsync limits.conf sysctl.conf $listener":."
     ssh $listener "sudo cp limits.conf /etc/security/limits.conf"
+    ssh $listener "sudo cp sysctl.conf /etc/sysctl.conf"
+    ssh $listener "sudo sysctl -p"
     ssh $listener "sudo apt-get install libzmq3-dev -y"
   }&
 done
